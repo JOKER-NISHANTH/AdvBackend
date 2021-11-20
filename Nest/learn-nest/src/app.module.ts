765da3901f8b85dcbe16760c95ad460f7aa30217
Nishanth <innocentnishanth@gmail.com>
2021-11-20 16:47:57 +0530

    URI Removed

--- Nest/learn-nest/src/app.module.ts
+++ Nest/learn-nest/src/app.module.ts
@@ -6,13 +6,14 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { ProductsService } from './products/products.service';
-import { ProductsController } from './products/products.controller';
+//import { ProductsService } from './products/products.service';
+//import { ProductsController } from './products/products.controller';
 import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [ProductsModule, MongooseModule.forRoot(
-    'MONGO_URI=mongodb+srv://JOKER-NISHANTH:NiShAnTh&007@cluster0.whmue.mongodb.net/NestProducts-demo?retryWrites=true&w=majority'
+    //! DataBase URI
+
   ),
 
   ],
